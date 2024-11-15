a07c9682e16971688f311abeec0a29895b88261f
BuildButterHub <155737260+BuildButterHub@users.noreply.github.com>
2024-11-15 22:01:35 +0300

    Update Creator.lua

--- src/Creator.lua
+++ src/Creator.lua
@@ -1,1 +1,11 @@
 local Icons = require("../Icons/Init.lua")
+local Creator = {}
+
+function Creator:Icon(Icon)
+    local filePath = "WindUI/Assets/spritesheet-new.png"
+    if not isfile(filePath) then
+        local response = game:HttpGet("https://raw.githubusercontent.com/Footagesus/WindUI/refs/heads/main/Lucide/Icons/spritesheet-new.png")
+        writefile(filePath, response)
+    end
+    return { getcustomasset(filePath), Icons[Icon] }
+end
