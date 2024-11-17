ad5bedb9878cae82d2573b6d565a1b22a2abf12c
BuildButterHub <155737260+BuildButterHub@users.noreply.github.com>
2024-11-17 15:17:11 +0300

    Create BloomTheme.lua

---
+++ src/Themes/BloomTheme.lua
@@ -0,0 +1,32 @@
+		BloomTheme = {
+			Background = Color3.fromRGB(149, 126, 145),
+			LeftBackground = Color3.fromRGB(167, 141, 163),
+			Text = Color3.fromRGB(255, 255, 255),
+			CreatorUI = Color3.fromRGB(216, 216, 216),
+			TabModule = {
+				TabEnable = Color3.fromRGB(255, 255, 255),
+				TabDisable = Color3.fromRGB(198, 198, 198), 
+			},
+			ElementColor = Color3.fromRGB(185, 156, 181),
+			ElementAnim = {
+				Old = Color3.fromRGB(185, 156, 181),
+				New = Color3.fromRGB(191, 162, 187),
+			},
+			ToggleModule = {
+				Toggle = Color3.fromRGB(149, 126, 145),
+				Scroll = Color3.fromRGB(116, 98, 113),
+				ScrollNew = Color3.fromRGB(204, 172, 199),
+			},
+			SliderModule = {
+				SliderBar = Color3.fromRGB(149, 126, 145),
+				SliderPart = Color3.fromRGB(226, 191, 220),
+				SliderValBG = Color3.fromRGB(149, 126, 145),
+				SliderValBGLabel = Color3.fromRGB(255, 255, 255),
+			},
+			Dropdown = {
+				DropValue = Color3.fromRGB(149, 126, 145),
+				DropOption = Color3.fromRGB(185, 156, 181),
+				OldAnim = Color3.fromRGB(185, 156, 181),
+				NewAnim = Color3.fromRGB(197, 168, 193),
+			}
+		}
