94f524f479d89126aaed90d49a179ac64f6655f3
BuildButterHub <155737260+BuildButterHub@users.noreply.github.com>
2024-11-17 15:16:51 +0300

    Create AmethystTheme.lua

---
+++ src/Themes/AmethystTheme.lua
@@ -0,0 +1,32 @@
+		AmethystTheme = {
+			Background = Color3.fromRGB(39, 0, 58),
+			LeftBackground = Color3.fromRGB(45, 0, 67),
+			Text = Color3.fromRGB(207, 207, 207),
+			CreatorUI = Color3.fromRGB(159, 159, 159),
+			TabModule = {
+				TabEnable = Color3.fromRGB(207, 207, 207),
+				TabDisable = Color3.fromRGB(141, 141, 141), 
+			},
+			ElementColor = Color3.fromRGB(51, 0, 76),
+			ElementAnim = {
+				Old = Color3.fromRGB(51, 0, 76),
+				New = Color3.fromRGB(57, 0, 82),
+			},
+			ToggleModule = {
+				Toggle = Color3.fromRGB(39, 0, 58),
+				Scroll = Color3.fromRGB(63, 0, 95),
+				ScrollNew = Color3.fromRGB(79, 0, 121),
+			},
+			SliderModule = {
+				SliderBar = Color3.fromRGB(39, 0, 58),
+				SliderPart = Color3.fromRGB(79, 0, 121),
+				SliderValBG = Color3.fromRGB(39, 0, 58),
+				SliderValBGLabel = Color3.fromRGB(207, 207, 207),
+			},
+			Dropdown = {
+				DropValue = Color3.fromRGB(39, 0, 58),
+				DropOption = Color3.fromRGB(51, 0, 76),
+				OldAnim = Color3.fromRGB(51, 0, 76),
+				NewAnim = Color3.fromRGB(63, 0, 88),
+			}
+		}
