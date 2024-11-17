f2efa497b378773da8d4e5cfe2de46e7231ccd0a
BuildButterHub <155737260+BuildButterHub@users.noreply.github.com>
2024-11-17 15:15:49 +0300

    Create DarkTheme.lua

---
+++ src/Themes/DarkTheme.lua
@@ -0,0 +1,32 @@
+		DarkTheme = {
+			Background = Color3.fromRGB(25, 25, 25),
+			LeftBackground = Color3.fromRGB(29, 29, 29),
+			Text = Color3.fromRGB(207, 207, 207),
+			CreatorUI = Color3.fromRGB(159, 159, 159),
+			TabModule = {
+				TabEnable = Color3.fromRGB(207, 207, 207),
+				TabDisable = Color3.fromRGB(141, 141, 141), 
+			},
+			ElementColor = Color3.fromRGB(31, 31, 31),
+			ElementAnim = {
+				Old = Color3.fromRGB(31, 31, 31),
+				New = Color3.fromRGB(37, 37, 37),
+			},
+			ToggleModule = {
+				Toggle = Color3.fromRGB(37, 37, 37),
+				Scroll = Color3.fromRGB(65, 65, 65),
+				ScrollNew = Color3.fromRGB(116, 116, 116),
+			},
+			SliderModule = {
+				SliderBar = Color3.fromRGB(37, 37, 37),
+				SliderPart = Color3.fromRGB(207, 207, 207),
+				SliderValBG = Color3.fromRGB(37, 37, 37),
+				SliderValBGLabel = Color3.fromRGB(207, 207, 207),
+			},
+			Dropdown = {
+				DropValue = Color3.fromRGB(37, 37, 37),
+				DropOption = Color3.fromRGB(31, 31, 31),
+				OldAnim = Color3.fromRGB(31, 31, 31),
+				NewAnim = Color3.fromRGB(43, 43, 43),
+			}
+		}
