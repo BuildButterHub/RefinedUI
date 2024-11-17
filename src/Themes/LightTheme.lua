b5d41a4743ea4f957260b069a6c42b6f05a7ff83
BuildButterHub <155737260+BuildButterHub@users.noreply.github.com>
2024-11-17 15:16:29 +0300

    Create LightTheme.lua

---
+++ src/Themes/LightTheme.lua
@@ -0,0 +1,32 @@
+    LightTheme = {
+			Background = Color3.fromRGB(84, 84, 84),
+			LeftBackground = Color3.fromRGB(88, 88, 88),
+			Text = Color3.fromRGB(207, 207, 207),
+			CreatorUI = Color3.fromRGB(159, 159, 159),
+			TabModule = {
+				TabEnable = Color3.fromRGB(207, 207, 207),
+				TabDisable = Color3.fromRGB(141, 141, 141), 
+			},
+			ElementColor = Color3.fromRGB(98, 98, 98),
+			ElementAnim = {
+				Old = Color3.fromRGB(98, 98, 98),
+				New = Color3.fromRGB(104, 104, 104),
+			},
+			ToggleModule = {
+				Toggle = Color3.fromRGB(84, 84, 84),
+				Scroll = Color3.fromRGB(116, 116, 116),
+				ScrollNew = Color3.fromRGB(188, 188, 188),
+			},
+			SliderModule = {
+				SliderBar = Color3.fromRGB(84, 84, 84),
+				SliderPart = Color3.fromRGB(207, 207, 207),
+				SliderValBG = Color3.fromRGB(84, 84, 84),
+				SliderValBGLabel = Color3.fromRGB(207, 207, 207),
+			},
+			Dropdown = {
+				DropValue = Color3.fromRGB(84, 84, 84),
+				DropOption = Color3.fromRGB(98, 98, 98),
+				OldAnim = Color3.fromRGB(98, 98, 98),
+				NewAnim = Color3.fromRGB(110, 110, 110),
+			}
+		}
